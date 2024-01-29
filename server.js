74d6b911c4c56d5ed7053ff800c961cc19bb7c7b
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2024-01-29 08:07:32 +0530

    updated backhend packages

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require("express");
 var cors = require("cors");
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
 const connect = require("./Config/db");
+
 const paymentRoutes = require("./routes/payments");
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orders');
@@ -12,9 +14,12 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 //create the app 
 var app = express();
-app.use(cors());
+const jsonParser = bodyParser.json();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(jsonParser);
+app.use(cors());
 
 app.use("/api/payment/", paymentRoutes);
 app.use("/api/user/", userRoutes);
